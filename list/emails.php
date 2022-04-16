8f9b46b9e575dd1e21da96dd059455485e68db6a
Martin Dieblich <martin.dieblich@gmx.de>
2022-04-16 20:52:24 +0200

    Email-Auflistung an andere Listen angepasst

--- list/emails.php
+++ list/emails.php
@@ -1,7 +1,8 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/class/email.php";
 
-foreach(require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/emails.php" as $email){
+$emails = require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/emails.php";
+foreach($emails as $email){
   echo $email->getDebugOutput();
 }
 
