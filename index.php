35079903dc31d3dc4dc951c8bb67eeee8e5ca579
Martin Dieblich <martin.dieblich@gmx.de>
2022-04-17 01:49:06 +0200

    index.php gekürzt (Steht jetzt alles wo anders)

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/db_connect.php";
 
-// $sql = "SELECT * FROM mannschaft";
-// $result = $mysqli->query($sql);
-
-// if ($result->num_rows > 0) {
-//   // output data of each row
-//   while($mannschaft = $result->fetch_assoc()) {
-//     $mannschaftObj = new Mannschaft($mannschaft);
-//     echo $mannschaftObj->getDebugOutput();
-//   }
-// } else {
-//   echo "0 results";
-// }
 $mysqli->close();
 ?>
