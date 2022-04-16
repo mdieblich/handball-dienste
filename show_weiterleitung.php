8b14e733465e55eafe55abb88b3b56d79fb07b27
Martin Dieblich <martin.dieblich@gmx.de>
2022-04-17 01:49:20 +0200

    anzeige der Personen pro Spiel

---
+++ show_weiterleitung.php
@@ -0,0 +1,25 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/class/email.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/class/dienst.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/db_connect.php";
+
+$emails = require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/emails.php";
+$mannschaften = require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/mannschaften.php";
+
+foreach($emails as $email){
+    echo "<b>".$email->getBisherZeile()."</b><br>";
+    $nuligaID = $email->getSpielNummer();
+    $sql = "SELECT * FROM dienst LEFT JOIN spiel ON dienst.spiel=spiel.id WHERE spiel.nuliga_id=".$nuligaID;
+    $result = $mysqli->query($sql);
+    if ($result->num_rows > 0) {
+        echo "<ul>";
+        while($dienst = $result->fetch_assoc()) {
+            $dienstObj = new Dienst($dienst);
+            echo "<li>".$dienstObj->getDebugOutput($mannschaften)."</li>";
+        }
+        echo "</ul>";
+    }
+}
+
+$mysqli->close();
+?>
