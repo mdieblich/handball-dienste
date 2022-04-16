ecd2020fdfc935ab9ec68cf845d8e47f7825c9b2
Martin Dieblich <martin.dieblich@gmx.de>
2022-04-16 19:13:09 +0200

    Laden aller Mannschaften in eigene Skript

--- index.php
+++ index.php
@@ -1,20 +1,22 @@
 <?php
-require_once "email.php";
+require_once "mannschaft.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/db_connect.php";
 
-$mysqli = require_once "db_connect.php";
-
-$sql = "SELECT * FROM email_inbox";
-$result = $mysqli->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($email = $result->fetch_assoc()) {
-    $emailObj = new Email($email["inhalt"]);
-    echo $emailObj->getDebugOutput();
-  }
-} else {
-  echo "0 results";
+foreach(require_once "load/mannschaften.php" as $mannschaft){
+  echo $mannschaft->getDebugOutput();
 }
 
+// $sql = "SELECT * FROM mannschaft";
+// $result = $mysqli->query($sql);
+
+// if ($result->num_rows > 0) {
+//   // output data of each row
+//   while($mannschaft = $result->fetch_assoc()) {
+//     $mannschaftObj = new Mannschaft($mannschaft);
+//     echo $mannschaftObj->getDebugOutput();
+//   }
+// } else {
+//   echo "0 results";
+// }
 $mysqli->close();
 ?>
---
+++ load/mannschaften.php
@@ -0,0 +1,18 @@
+<?php
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/mannschaft.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/db_connect.php";
+
+$sql = "SELECT * FROM mannschaft";
+$result = $mysqli->query($sql);
+
+$mannschaften = array();
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($mannschaft = $result->fetch_assoc()) {
+    array_push($mannschaften, new Mannschaft($mannschaft));
+  }
+}
+
+
+return $mannschaften;
+?>
