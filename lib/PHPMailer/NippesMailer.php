4ac009adac0c4e8d470e532715c26ff6e158275b
Martin Dieblich <martin.dieblich@gmx.de>
2022-04-17 15:59:45 +0200

    NippesMailer um PHPMailer herumgebastelt

---
+++ lib/PHPMailer/NippesMailer.php
@@ -0,0 +1,36 @@
+<?php
+// PHPMailer
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/src/Exception.php";
+require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/src/PHPMailer.php";
+require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/src/SMTP.php";
+
+function init_nippes_mailer(): PHPMailer {
+
+    $mail = new PHPMailer(true);
+
+    //Server settings
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.ionos.de';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'dienstebot@turnerkreisnippes.de';
+    $mail->Password   = '9nUgcLpcRMz3fLF';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
+    //Recipients
+    $mail->setFrom('dienstebot@turnerkreisnippes.de', 'Nippes Bot');
+    $mail->addAddress('martin.dieblich@gmx.de', 'Martin Dieblich');
+    // $mail->addBCC('bcc@example.com');
+    $mail->addReplyTo('martin.dieblich@gmx.de', 'Martin Dieblich');
+
+    //Content
+    $mail->isHTML(true);
+
+    return $mail;
+
+}
+?>
--- script/send_weiterleitungen.php
+++ script/send_weiterleitungen.php
@@ -4,33 +4,20 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
-require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/src/Exception.php";
-require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/src/PHPMailer.php";
-require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/src/SMTP.php";
+require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/lib/PHPMailer/NippesMailer.php";
 
 // Datenbank-Zugriff
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/db_connect.php";
 
-$mail = new PHPMailer(true);
 
 try {
-    //Server settings
-    $mail->isSMTP();
-    $mail->Host       = 'smtp.ionos.de';
-    $mail->SMTPAuth   = true;
-    $mail->Username   = 'dienstebot@turnerkreisnippes.de';
-    $mail->Password   = '9nUgcLpcRMz3fLF';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port       = 587;
+    $mail = init_nippes_mailer();
 
     //Recipients
-    $mail->setFrom('dienstebot@turnerkreisnippes.de', 'Nippes Bot');
     $mail->addAddress('martin.dieblich@gmx.de', 'Martin Dieblich');
     // $mail->addBCC('bcc@example.com');
-    $mail->addReplyTo('mdieblich@gmail.com', 'Martin GOOGLE Dieblich');
 
     //Content
-    $mail->isHTML(true);
     $mail->Subject = 'Spielverlegung bei deinem Zeitnehmer-Dienst';
     $mail->Body    = 'Das Spiel wurde verlegt! <b>Denk dran!</b>';
     $mail->AltBody = 'Das Spiel wurde verlegt! Denk dran!';
