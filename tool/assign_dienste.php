b918c9de4900ca107ad8c2dd5a4e9da0584925fb
Martin Dieblich <martin.dieblich@gmx.de>
2022-05-09 20:56:23 +0200

    Tabelle zum Zuweisen von Diensten erstellt

--- tool/assign_dienste.php
+++ tool/assign_dienste.php
@@ -4,12 +4,57 @@ require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/mannschaften.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/spiele.php";
 
 $mannschaften = loadMannschaften();
-$spiele = loadSpiele("1=1", "anwurf, mannschaft"); 
+$spiele = loadSpieleDeep("1=1", "anwurf, mannschaft"); 
+
+function findeGleichzeitigesSpiel(Spiel $zuVergleichendesSpiel, Mannschaft $mannschaft): ?Spiel{
+    global $spiele;
+    foreach($spiele as $spiel){
+        if($spiel->getMannschaft() != $mannschaft->getID()){
+            continue; // Kein Spiel der Mannschaft
+        }
+    }
+    return null;
+}
 
 ?>
+<script>
+    function assignDienst(spiel, dienstart, mannschaft, assign){
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            if (this.readyState != 4) return;
+
+            if (this.status == 200) {
+                console.log(this.responseText);
+            }
+        };
+        xhr.open(assign?"PUT":"DELETE", "../api/dienst.php", true);
+        var dienst = new Object();
+        dienst.spiel = spiel;
+        dienst.dienstart = dienstart;
+        dienst.mannschaft = mannschaft;
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        //console.log(JSON.stringify(dienst));
+        xhr.send(JSON.stringify(dienst));
+        disableOtherCheckboxes(spiel, dienstart, mannschaft, assign);
+    }
+    function disableOtherCheckboxes(spiel, dienstart, mannschaft, assign){
+        checkBoxName = dienstart+"-"+spiel;
+        console.log("Suche nach " + checkBoxName + " Setze disabled auf " + !assign);
+        otherCheckBoxes = document.getElementsByName(checkBoxName);
+        console.log(otherCheckBoxes);
+        for(i=0; i<otherCheckBoxes.length; i++){
+            otherCheckBoxes[i].disabled = assign;
+        }
+        // immer die aktive CheckBox aktivieren
+        activeID = checkBoxName+"-"+mannschaft;
+        document.getElementById(activeID).disabled = false;
+    }
+</script>
 <table border="1" cellpadding="3" cellspacing="3">
     <tr>
+        <th>ID</th>
         <th>Datum</th>
+        <th>Mannschaft</th>
         <th>Gegner</th>
         <th>Heimspiel</th>
 <?php
@@ -19,17 +64,79 @@ foreach($mannschaften as $mannschaft){
 ?>
     </tr>
 <?php
+
+function getGleichzeitigesSpiel($zuPruefendesSpiel, $mannschaft): ?Spiel{
+    global $spiele;
+    // TODO das geht definitiv einfacher: Alle Spiele als Array in Mannschaft
+    foreach($spiele as $spiel){
+        if($spiel->getMannschaft() != $mannschaft->getID()){
+            continue;
+        }
+        if($spiel->isGleichzeitig($zuPruefendesSpiel)){
+            return $spiel;
+        }
+    }
+    return null;
+}
+
 foreach($spiele as $spiel){
+    $zeitnehmerDienst = $spiel->getDienst("Zeitnehmer");
+    $sekretaerDienst = $spiel->getDienst("Sekretär");
     echo "<tr>";
+    echo "<td>".$spiel->getID()."</td>";
     echo "<td>".$spiel->getAnwurf()->format('d.m.Y H:i')."</td>";
+    echo "<td>".$mannschaften[$spiel->getMannschaft()]->getName()."</td>";
     echo "<td>".$spiel->getGegner()."</td>";
     echo "<td align=\"center\">".($spiel->isHeimspiel()?"Ja":"Nein")."</td>";
     foreach($mannschaften as $mannschaft){
-        $checkBoxID = $spiel->getNuligaID()."-".$mannschaft->getID();
-        echo "<td>";
-        echo "<input type=\"checkbox\" id=\"Z-$checkBoxID\"><label for=\"Z-$checkBoxID\">Z</label><br>";
-        echo "<input type=\"checkbox\" id=\"S-$checkBoxID\"><label for=\"S-$checkBoxID\">S</label><br>";
-        echo "</td>";
+        if($spiel->getMannschaft() == $mannschaft->getID()){
+            // TODO Override ermöglichen, sodass dies doch möglich wird
+            echo "<td align=\"center\" title=\"Eigenes Spiel\">X</td>";
+            continue;
+        }
+        $gleichzeitigesSpiel = getGleichzeitigesSpiel($spiel, $mannschaft);
+        if(isset($gleichzeitigesSpiel)) {
+            // TODO Override ermöglichen, sodass dies doch möglich wird
+            echo "<td align=\"center\" title=\"Gleichzeitiges Spiel, ID ".$gleichzeitigesSpiel->getID()."\">X</td>";
+        } else {
+            $checkBoxID = $spiel->getID()."-".$mannschaft->getID();
+            echo "<td>";
+            $zeitnehmerChecked = "";
+            if(isset($zeitnehmerDienst)){
+                if( $zeitnehmerDienst->getMannschaft() == $mannschaft->getID()){
+                    // wir haben den Dienst!
+                    $zeitnehmerChecked = "checked";
+                }
+                else{
+                    // eine andere Mannschaft hat den Dienst
+                    $zeitnehmerChecked = "disabled";
+                }
+            }
+            echo "<input type=\"checkbox\" ".
+                "name=\"Zeitnehmer-".$spiel->getID()."\"".
+                "id=\"Zeitnehmer-$checkBoxID\" ".
+                "onclick=\"assignDienst(".$spiel->getID().",'Zeitnehmer',".$mannschaft->getID().", this.checked)\"".
+                " $zeitnehmerChecked>".
+                "<label for=\"Zeitnehmer-$checkBoxID\">Z</label><br>";
+            
+            $sekretaerChecked = "";
+            if(isset($sekretaerDienst)){
+                if($sekretaerDienst->getMannschaft() == $mannschaft->getID()){
+                    // wir haben den Dienst!
+                    $sekretaerChecked = "checked";
+                } else{
+                    // eine andere Mannschaft hat den Dienst
+                    $sekretaerChecked = "disabled";
+                }
+            }
+            echo "<input type=\"checkbox\" ".
+                "name=\"Sekretär-".$spiel->getID()."\"".
+                "id=\"Sekretär-$checkBoxID\" ".
+                "onclick=\"assignDienst(".$spiel->getID().",'Sekretär',".$mannschaft->getID().", this.checked)\"".
+                " $sekretaerChecked>".
+                "<label for=\"Sekretär-$checkBoxID\">S</label><br>";
+            echo "</td>";
+        }
     }
     echo "</tr>";
 }
