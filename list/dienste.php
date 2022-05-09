8f624a148a65097393f352e5a220f85eee1d6091
Martin Dieblich <martin.dieblich@gmx.de>
2022-05-09 20:55:14 +0200

    Laden der Dienste über Funktionsaufruf

--- list/dienste.php
+++ list/dienste.php
@@ -1,7 +1,8 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/entity/dienst.php";
+require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/dienste.php";
 
-$dienste = require $_SERVER['DOCUMENT_ROOT']."/dienstedienst/load/dienste.php";
+$dienste = loadAllDienste(); 
 foreach($dienste as $dienst){
   echo $dienst->getDebugOutput();
 }
--- load/dienste.php
+++ load/dienste.php
@@ -2,16 +2,19 @@
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/entity/dienst.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/dienstedienst/db_connect.php";
 
-$sql = "SELECT * FROM dienst ORDER BY spiel, dienstart";
-$result = $mysqli->query($sql);
+function loadAllDienste(){
+  global $mysqli;
+  $sql = "SELECT * FROM dienst ORDER BY spiel, dienstart";
+  $result = $mysqli->query($sql);
 
-$dienste = array();
-if ($result->num_rows > 0) {
-  while($dienst = $result->fetch_assoc()) {
-    $dienstObj = new Dienst($dienst);
-    $dienste[$dienstObj->getID()] = $dienstObj;
+  $dienste = array();
+  if ($result->num_rows > 0) {
+    while($dienst = $result->fetch_assoc()) {
+      $dienstObj = new Dienst($dienst);
+      $dienste[$dienstObj->getID()] = $dienstObj;
+    }
   }
-}
 
-return $dienste;
+  return $dienste;
+}
 ?>
