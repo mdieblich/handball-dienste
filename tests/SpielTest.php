5e4386731a04902aa165d1a066f6d2f3e5480b17
Martin Dieblich <martin.dieblich@gmx.de>
2022-05-10 18:17:20 +0200

    Tests um Halle erweitert

--- tests/SpielTest.php
+++ tests/SpielTest.php
@@ -19,8 +19,8 @@ function assertNichtGleichzeitig(Spiel $a, Spiel $b){
 
 echo "Gleichzeitigkeit: ";
 {
-    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00"));
+    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"1000"));
+    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"1000"));
     assertGleichzeitig($a, $b);
 }
 echo "<hr>";
@@ -29,32 +29,32 @@ echo "<h1>Heimspiel-Vergleiche</h1>";
 
 echo "Später, überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 17:00:00"));
+    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"1000"));
+    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 16:20:00", "halle"=>"1000"));
     assertGleichzeitig($a, $b);
 }
 echo "<hr>";
 
 echo "Später, nicht überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 19:00:00"));
+    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"1000"));
+    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 19:00:00", "halle"=>"1000"));
     assertNichtGleichzeitig($a, $b);
 }
 echo "<hr>";
 
 echo "Früher, überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 13:00:00"));
+    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"1000"));
+    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 13:40:00", "halle"=>"1000"));
     assertGleichzeitig($a, $b);
 }
 echo "<hr>";
 
 echo "Früher, nicht überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 11:00:00"));
+    $a = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"1000"));
+    $b = new Spiel(array("heimspiel"=>"1", "anwurf"=>"2022-05-01 11:00:00", "halle"=>"1000"));
     assertNichtGleichzeitig($a, $b);
 }
 echo "<hr>";
@@ -63,32 +63,32 @@ echo "<hr>";
 echo "<h1>Auswärtsspiel-Vergleiche</h1>";
 echo "Später, überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 20:00:00"));
+    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"2000"));
+    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 20:00:00", "halle"=>"3000"));
     assertGleichzeitig($a, $b);
 }
 echo "<hr>";
 
 echo "Später, nicht überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 21:00:00"));
+    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"2000"));
+    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 21:00:00", "halle"=>"3000"));
     assertNichtGleichzeitig($a, $b);
 }
 echo "<hr>";
 
 echo "Früher, überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 10:00:00"));
+    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"2000"));
+    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 10:00:00", "halle"=>"3000"));
     assertGleichzeitig($a, $b);
 }
 echo "<hr>";
 
 echo "Früher, nicht überlappend: ";
 {
-    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00"));
-    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 09:00:00"));
+    $a = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 15:00:00", "halle"=>"2000"));
+    $b = new Spiel(array("heimspiel"=>"0", "anwurf"=>"2022-05-01 09:00:00", "halle"=>"3000"));
     assertNichtGleichzeitig($a, $b);
 }
 echo "<hr>";
