e6a54ef4b473a2e7fa6411efb20e9e08d5390e05
Martin Dieblich <martin.dieblich@gmx.de>
2022-05-13 16:25:58 +0200

    ZeitRaum-Klasse angelegt

--- entity/ZeitKlassen.php
+++ entity/ZeitKlassen.php
@@ -1,5 +1,15 @@
 <?php
 
+class ZeitRaum {
+    public DateTime $start;
+    public DateTime $ende;
+
+    public function __construct(DateTime $start, DateTime $ende) {
+        $this->start = $start;
+        $this->ende = $ende;
+    }
+}
+
 class ZeitlicheDistanz {
     public bool $ueberlappend;
     public bool $vorher;
@@ -8,16 +18,23 @@ class ZeitlicheDistanz {
     public static function fromZeitraeumen(
         DateTime $start_a, DateTime $ende_a, 
         DateTime $start_b, DateTime $ende_b): ZeitlicheDistanz{
+
+        $a = new ZeitRaum($start_a, $ende_a);
+        $b = new ZeitRaum($start_b, $ende_b);
+        return self::fromZeitRaeumen2($a, $b);
+    }
+    
+    public static function fromZeitRaeumen2(ZeitRaum $a, ZeitRaum $b): ZeitlicheDistanz{
         
         $distanz = new ZeitlicheDistanz();
-        $distanz->ueberlappend = $ende_a > $start_b && $ende_b > $start_a;
-        $distanz->vorher = $start_b < $start_a;
+        $distanz->ueberlappend = $a->ende > $b->start && $b->ende > $a->start;
+        $distanz->vorher = $b->start < $a->start;
         if($distanz->vorher){
             // anderes Spiel ist vorher
-            $distanz->abstand = $start_a->diff($ende_b);
+            $distanz->abstand = $a->start->diff($b->ende);
         } else { 
             // anderes Spiel ist nachher
-            $distanz->abstand = $ende_a->diff($start_b);
+            $distanz->abstand = $a->ende->diff($b->start);
         }
         return $distanz;
     }
