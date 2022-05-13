d7a460e8aeb3e54dec3d7bdb9d915a9f458b6deb
Martin Dieblich <martin.dieblich@gmx.de>
2022-05-13 22:58:14 +0200

    "E" und "G" wider mittig

--- tool/assign_dienste.php
+++ tool/assign_dienste.php
@@ -159,7 +159,7 @@ foreach($spiele as $spiel){
         if($spiel->getMannschaft() == $mannschaft->getID()){
             // TODO Override ermöglichen, sodass dies doch möglich wird
             $textColor = "silver";
-            echo "<td style=\"background-color:$backgroundColor; color:$textColor; text-align:center\" title=\"Eigenes Spiel\"><br><br>E</td>";
+            echo "<td style=\"background-color:$backgroundColor; color:$textColor; text-align:center\" title=\"Eigenes Spiel\">E</td>";
             continue;
         }
         $zeitlichNaehstesSpiel = getZeitlichNaehstesSpiel($spiel, $mannschaft);
@@ -169,8 +169,7 @@ foreach($spiele as $spiel){
             // TODO Zelle mal als Klasse extrahieren
             $textColor = "silver";
             echo "<td style=\"background-color:$backgroundColor; color:$textColor; text-align:center\" title=\"Gleichzeitiges Spiel, ID ".$zeitlichNaehstesSpiel->getID()."\">";
-            echo $zeitlicheDistanz->seconds / 3600;
-            echo "<br><br>G</td>";
+            echo "G</td>";
         } else {
             if(isAmGleichenTag($spiel, $zeitlichNaehstesSpiel)){
                 $backgroundColor = "#ffd";
