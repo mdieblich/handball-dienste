922b9e056d284ae6c1d7eec9766f192e9a19777e
Martin Dieblich <martin.dieblich@gmx.de>
2022-05-29 13:22:33 +0200

    Leeres Plugin

---
+++ wordpress/dienstedienst.php
@@ -0,0 +1,9 @@
+<?php
+ /*
+ Plugin Name: Dienstedienst
+ Description: Plugin zum Verwalten zusätzlicher Dienste (Zeitnehmer, Catering...) beim Handball
+ Version: 1.0
+ Author: Martin Dieblich
+ Author URI: https://www.turnerkreisnippes.de
+ */
+?>
