6c20da7d561f402b4ed02b8e0300b8654e703b8b
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-09 00:40:22 +0200

    Menüs ausgelagert

--- wordpress/dienstedienst.php
+++ wordpress/dienstedienst.php
@@ -7,21 +7,8 @@
  Author URI: https://www.turnerkreisnippes.de
  */
 require_once plugin_dir_path( __FILE__ ) . 'einstellungen.php';
-
-add_action('admin_menu', 'addDiensteMenueeintraege');
-
-function addDiensteMenueeintraege() {
-    //add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-    add_menu_page(  'Dienste', 'Dienste', 'administrator', 'dienste', 'displayDiensteDashboard', 'dashicons-schedule' );
-    
-    //add_submenu_page( '$parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
-    add_submenu_page( 'dienste', 'Dienste - Mannschaften einrichten', 'Mannschaften', 'administrator', 'dienste-mannschaften', 'displayDiensteMannschaften');
-    add_submenu_page( 'dienste', 'Dienste zuweisen', 'Dienste zuweisen', 'administrator', 'dienste-zuweisen', 'displayDiensteZuweisen');
-}
-
-function displayDiensteDashboard(){}
-function displayDiensteMannschaften(){}
-function displayDiensteZuweisen(){}
+require_once plugin_dir_path( __FILE__ ) . 'menues.php';
 
 add_action('admin_init', 'dienste_einstellungen_initialisieren');
+add_action('admin_menu', 'addDiensteMenueeintraege');
 ?>
---
+++ wordpress/menues.php
@@ -0,0 +1,16 @@
+<?php
+
+function addDiensteMenueeintraege() {
+    //add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+    add_menu_page(  'Dienste', 'Dienste', 'administrator', 'dienste', 'displayDiensteDashboard', 'dashicons-schedule' );
+    
+    //add_submenu_page( '$parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
+    add_submenu_page( 'dienste', 'Dienste - Mannschaften einrichten', 'Mannschaften', 'administrator', 'dienste-mannschaften', 'displayDiensteMannschaften');
+    add_submenu_page( 'dienste', 'Dienste zuweisen', 'Dienste zuweisen', 'administrator', 'dienste-zuweisen', 'displayDiensteZuweisen');
+}
+
+function displayDiensteDashboard(){}
+function displayDiensteMannschaften(){}
+function displayDiensteZuweisen(){}
+
+?>
