04ba3e0f0ff88f71025f7692d1f16c86ccab3bb5
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-10 00:24:54 +0200

    gesamten Plugin-Ordner zu Wordpress kopieren

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-WORDPRESS="E:\wamp64\www\wordpress\wp-content\plugins\dienstedienst"
+WORDPRESS="E:\wamp64\www\wordpress\wp-content\plugins"
 
 cd ./build
 
@@ -16,4 +16,4 @@ cp -r ../entity dienstedienst
 ./7za.exe a -r dienstedienst.zip dienstedienst
 
 # 4. Zusätzlich in's Wordpress-Verzeichnis kopieren, damit nicht manuell installiert werden muss.
-cp dienstedienst/*.* $WORDPRESS
+cp -r dienstedienst $WORDPRESS
