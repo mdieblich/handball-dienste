e998600544cbace3aa3900b4c65e01cbae084bd6
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-13 19:11:01 +0200

    einfügen und löschen eines Dienstes

--- dao/dienst.php
+++ dao/dienst.php
@@ -3,7 +3,7 @@ require_once WP_PLUGIN_DIR."/dienstedienst/entity/dienst.php";
 
 class DienstDAO{
 
-    public function loadAllDienste(): array{
+    public function loadAllDienste(string $whereClause="1=1", string $orderBy="id ASC"): array{
         global $wpdb;
   
         $table_name = $wpdb->prefix . 'dienst';
@@ -18,6 +18,37 @@ class DienstDAO{
             }
         }
         return $dienste;
+    }
+
+    public function insert(int $spiel, string $dienstart, int $mannschaft){
+        
+        global $wpdb;
+            
+        $table_name = $wpdb->prefix . 'dienst';
+
+        $params = array(
+            'spiel' => $spiel,
+            'dienstart' => $dienstart,
+            'mannschaft' => $mannschaft
+        );
+
+        $wpdb->insert($table_name, $params);
+  
+    }
+    public function delete(int $spiel, string $dienstart, int $mannschaft){
+        
+        global $wpdb;
+            
+        $table_name = $wpdb->prefix . 'dienst';
+
+        $params = array(
+            'spiel' => $spiel,
+            'dienstart' => $dienstart,
+            'mannschaft' => $mannschaft
+        );
+
+        $wpdb->delete($table_name, $params);
+  
     }
 }
 
