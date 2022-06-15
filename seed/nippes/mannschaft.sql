1416c727694087cd149286d2996bbff4a3c22012
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-15 14:41:30 +0200

    altes gelöscht

--- seed/nippes/mannschaft.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO `mannschaft` (`id`, `nummer`, `geschlecht`, `meisterschaft`, `liga`, `nuliga_liga_id`, `nuliga_team_id`) VALUES
-(1, 1, 'm', 'MR 21/22', 'Mittelrhein Oberliga Männer', 274529, 1744276),
-(2, 1, 'w', 'MR 21/22', 'Mittelrhein Oberliga Frauen', 274482, 1748577),
-(3, 2, 'm', 'KR 21/22', 'Kreisliga Männer',            274464, 1744462),
-(4, 2, 'w', 'KR 21/22', 'Kreisliga Frauen',            274439, 1744465),
-(5, 3, 'm', 'KR 21/22', 'Kreisklasse 2 Männer',        274480, 1744461),
-(6, 3, 'w', 'KR 21/22', 'Kreisklasse Frauen',          274567, 1746321);
