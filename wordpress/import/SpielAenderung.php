f8fdd16b3e83ddba02881dcb09e6a9e74f417b00
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-15 13:31:57 +0200

    SpielAenderung in Import verschoben

---
+++ wordpress/import/SpielAenderung.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__."/../entity/spiel.php";
+require_once __DIR__."/../grabber/NuLigaSpiel.php";
+require_once __DIR__."/../dao/gegner.php";
+
+class SpielAenderung{
+    public Spiel $alt;
+    public NuLigaSpiel $neu;
+    
+    public function __construct(Spiel $alt, NuLigaSpiel $neu){
+        $this->alt = $alt;
+        $this->neu = $neu;
+    }
+
+    public function getBegegnungsbezeichnung(array $alleMannschaften, GegnerDAO $gegnerDAO): string{
+        $message = "";
+        $mannschaft = $alleMannschaften[$this->alt->getMannschaft()]->getName();
+        $gegner = $gegnerDAO->fetch($this->alt->getGegner())->getName();
+        
+        if($this->alt->isHeimspiel()){
+            return "HEIM $mannschaft vs. $gegner";
+        } else{
+            return "AUSWÄRTS $gegner vs. $mannschaft";
+        }   
+    }
+
+    public function getAenderung(): string{
+        $message = "";
+        if($this->alt->getAnwurf() != $this->neu->getAnwurf()){
+
+            $message .= "Anwurf "
+                ."von [".$this->alt->getAnwurf()->format("d.m.Y H:i")."]"
+                ." zu [".$this->neu->getAnwurf()->format("d.m.Y H:i")."]";
+        }
+        if($this->alt->getHalle() != $this->neu->getHalle()){
+            $message .= " Halle "
+                ."von [".$this->alt->getHalle()."]"
+                ." zu [".$this->neu->getHalle()."]";
+        }
+        return $message;
+    }
+}
+?>
--- wordpress/import/importer.php
+++ wordpress/import/importer.php
@@ -3,44 +3,7 @@ require_once __DIR__."/../dao/mannschaft.php";
 require_once __DIR__."/../dao/gegner.php";
 require_once __DIR__."/../dao/spiel.php";
 require_once __DIR__."/../dao/dienst.php";
-
-class SpielAenderung{
-    public Spiel $alt;
-    public NuLigaSpiel $neu;
-    
-    public function __construct(Spiel $alt, NuLigaSpiel $neu){
-        $this->alt = $alt;
-        $this->neu = $neu;
-    }
-
-    public function getBegegnungsbezeichnung(array $alleMannschaften, GegnerDAO $gegnerDAO): string{
-        $message = "";
-        $mannschaft = $alleMannschaften[$this->alt->getMannschaft()]->getName();
-        $gegner = $gegnerDAO->fetch($this->alt->getGegner())->getName();
-        
-        if($this->alt->isHeimspiel()){
-            return "HEIM $mannschaft vs. $gegner";
-        } else{
-            return "AUSWÄRTS $gegner vs. $mannschaft";
-        }   
-    }
-
-    public function getAenderung(): string{
-        $message = "";
-        if($this->alt->getAnwurf() != $this->neu->getAnwurf()){
-
-            $message .= "Anwurf "
-                ."von [".$this->alt->getAnwurf()->format("d.m.Y H:i")."]"
-                ." zu [".$this->neu->getAnwurf()->format("d.m.Y H:i")."]";
-        }
-        if($this->alt->getHalle() != $this->neu->getHalle()){
-            $message .= " Halle "
-                ."von [".$this->alt->getHalle()."]"
-                ." zu [".$this->neu->getHalle()."]";
-        }
-        return $message;
-    }
-}
+require_once __DIR__."/SpielAenderung.php";
 
 class DienstAenderungsPlan{
     private DienstDAO $dao;
