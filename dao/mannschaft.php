f8ef3c6d58778bfe32ddfc98a5ff041eb270ca0f
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-16 12:50:02 +0200

    mannschaft über Namen finden

--- dao/mannschaft.php
+++ dao/mannschaft.php
@@ -2,19 +2,28 @@
 require_once WP_PLUGIN_DIR."/dienstedienst/entity/mannschaft.php";
 
 function loadMannschaften(): array{
-  global $wpdb;
-  
-  $table_name = $wpdb->prefix . 'mannschaft';
-  $sql = "SELECT * FROM $table_name ORDER BY nummer, geschlecht";
-  $result = $wpdb->get_results($sql, ARRAY_A);
+    global $wpdb;
+    
+    $table_name = $wpdb->prefix . 'mannschaft';
+    $sql = "SELECT * FROM $table_name ORDER BY nummer, geschlecht";
+    $result = $wpdb->get_results($sql, ARRAY_A);
 
-  $mannschaften = array();
-  if (count($result) > 0) {
-    foreach($result as $mannschaft) {
-      $mannschaftObj = new Mannschaft($mannschaft);
-      $mannschaften[$mannschaftObj->getID()] = $mannschaftObj;
+    $mannschaften = array();
+    if (count($result) > 0) {
+        foreach($result as $mannschaft) {
+            $mannschaftObj = new Mannschaft($mannschaft);
+            $mannschaften[$mannschaftObj->getID()] = $mannschaftObj;
+        }
     }
-  }
-  return $mannschaften;
+    return $mannschaften;
+}
+
+function getMannschaftFromName(array $mannschaften, string $name): ?Mannschaft{
+    foreach($mannschaften as $mannschaft){
+        if($mannschaft->getName() === $name){
+            return $mannschaft;
+        }
+    }
+    return null;
 }
 ?>
