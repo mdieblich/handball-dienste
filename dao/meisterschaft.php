4763f89c86c2efa335743f9b5a277146bf968e04
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-21 21:49:24 +0200

    typo in sql

--- dao/meisterschaft.php
+++ dao/meisterschaft.php
@@ -4,8 +4,8 @@ require_once WP_PLUGIN_DIR."/dienstedienst/entity/meisterschaft.php";
 function loadMeisterschaften(string $where = "1=1", string $orderby = "id"): array{
     global $wpdb;
     
-    $table_name = $wpdb->prefix . 'meiterschaft';
-    $sql = "SELECT * FROM $table_name WHER $where ORDER BY $orderby";
+    $table_name = $wpdb->prefix . 'meisterschaft';
+    $sql = "SELECT * FROM $table_name WHERE $where ORDER BY $orderby";
     $result = $wpdb->get_results($sql, ARRAY_A);
 
     $meisterschaften = array();
