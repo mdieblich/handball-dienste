9132dbe9a4b712a44fe59f46594f404a9e78321e
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-21 20:34:58 +0200

    Meisterschaft wird in Datenbank migriert

--- wordpress/datenbank_initialisieren.php
+++ wordpress/datenbank_initialisieren.php
@@ -3,17 +3,25 @@
 require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
 global $dienste_db_version;
-$dienste_db_version = '1.2';
+$dienste_db_version = '1.3';
 
 function dienste_datenbank_initialisieren() {
     global $dienste_db_version;
 
-    dienste_mannschaft_initialisieren();
+    $previous_version = get_option('dienste_db_version');
+    if($previous_version && $previous_version < '1.3'){
+        dienste_meisterschaft_initialisieren();
+        dienste_migrate_meisterschaft();
+        dienste_mannschaft_aktualisiern();
+    } else {
+        dienste_mannschaft_initialisieren();
+    }
+
     dienste_gegner_initialisieren();
     dienste_spiele_initialisieren();
     dienste_zuweisungen_initialisieren();
 
-    add_option( 'dienste_db_version', $dienste_db_version );
+    update_option( 'dienste_db_version', $dienste_db_version );
 }
 
 function dienste_mannschaft_initialisieren(){
@@ -27,15 +35,58 @@ function dienste_mannschaft_initialisieren(){
         nummer INT NOT NULL , 
         geschlecht enum('m','w') NOT NULL, 
         jugendklasse VARCHAR(256) NULL, 
-        email VARCHAR(1024) NULL , 
-        meisterschaft VARCHAR(256) NULL , 
-        liga VARCHAR(256) NULL , 
-        nuliga_liga_id INT NULL , 
-        nuliga_team_id INT NULL , 
+        email VARCHAR(1024) NULL ,
         PRIMARY KEY (id)
     ) $charset_collate, ENGINE = InnoDB;";
 
     dbDelta( $sql );
+}
+
+function dienste_meisterschaft_initialisieren(){
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'meisterschaft';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    $sql = "CREATE TABLE $table_name (
+        id INT NOT NULL AUTO_INCREMENT , 
+        name VARCHAR(1024) NOT NULL,
+        kuerzel VARCHAR(256) NOT NULL,
+        mannschaft INT NOT NULL , 
+        liga VARCHAR(256) NULL , 
+        nuliga_liga_id INT NULL , 
+        nuliga_team_id INT NULL , 
+        PRIMARY KEY (id), 
+        FOREIGN KEY (mannschaft) REFERENCES ".$wpdb->prefix."mannschaft(id) ON DELETE CASCADE ON UPDATE CASCADE
+    ) $charset_collate, ENGINE = InnoDB;";
+
+    dbDelta( $sql );
+}
+
+function dienste_migrate_meisterschaft(){
+    global $wpdb;
+
+    $table_name_mannschaft    = $wpdb->prefix . 'mannschaft';
+    $table_name_meisterschaft = $wpdb->prefix . 'meisterschaft';
+    $sql = "INSERT INTO $table_name_meisterschaft 
+        (name, kuerzel, mannschaft, liga, nuliga_liga_id, nuliga_team_id) 
+        SELECT meisterschaft, meisterschaft, id, liga, nuliga_liga_id, nuliga_team_id 
+        FROM $table_name_mannschaft";
+
+    $wpdb->query($sql);
+}
+
+function dienste_mannschaft_aktualisiern(){
+    global $wpdb;
+
+    $table_name    = $wpdb->prefix . 'mannschaft';
+    $sql = "ALTER TABLE $table_name 
+        DROP meisterschaft, 
+        DROP liga, 
+        DROP nuliga_liga_id, 
+        DROP nuliga_team_id";
+
+    $wpdb->query($sql);
 }
 
 function dienste_gegner_initialisieren(){
