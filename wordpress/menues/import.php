5c2b718786758b628c43a9b3e064eed03488a72b
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-23 19:13:35 +0200

    starten des Imports per ajax

--- wordpress/menues/import.php
+++ wordpress/menues/import.php
@@ -5,13 +5,40 @@ function addDiensteSpieleImportKonfiguration(){
     
     add_action( 'load-' . $hook_import, 'diensteImportSubmit' );
 }
+add_action( 'wp_ajax_meisterschaften_importieren', 'meisterschaften_importieren' );
 
 function displaySpieleImport(){
     require_once __DIR__."/../dao/mannschaft.php";
     require_once __DIR__."/../dao/spiel.php";
-    $mannschaften = loadMannschaften();
- ?>
+    $mannschaften = loadMannschaftenMitMeisterschaften();
+?>
+<script>
+function startImport(){
+    jQuery(function($){
+        $("#loading-spinner").show(500);
+    });
+
+    var data = {'action': 'meisterschaften_importieren'};
+
+    // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+    jQuery.post(ajaxurl, data, function(response){    
+        jQuery(function($){
+            $("#loading-spinner").hide(500, function(){
+                $("#import-result").show(500);
+            });
+            $("#import-result").html("<pre>" + response + "</pre>");
+        });
+    });
+}
+</script>
 <div class="wrap">
+
+    <div class="spinner-border" role="status" id="loading-spinner" style="display:none">
+        <span class="visually-hidden">Loading...</span>
+    </div>
+    
+    <div id="import-result" style="display:none">Ergebnisse!</div>
+
 
     <h1>Spiele von nuLiga importieren</h1>
     Einfach auf <i>"Importieren"</i> klicken:
@@ -21,6 +48,7 @@ function displaySpieleImport(){
         <li>Sollte sich ein Spiel ändern (Anwurf oder Halle), bei dem eine Mannschaft schon Dienste zugewiesen bekommen hat, dann bekommt diese eine Email.</li>
         <li>Auch bei mehreren sich ändernden Spielen bekommt eine Mannschaft pro Import immer nur genau <u>eine</u> Email. <i>(Ich hasse zu viele Emails!)</i>
     </ol>
+
     <table>
         <tr>
             <th> Mannschaft </th>
@@ -33,10 +61,11 @@ function displaySpieleImport(){
         </tr>
         <?php } ?>
     </table>
+    <button class="btn btn-primary" onclick="startImport()">Meisterschaften importieren</button>
     <form action="<?php menu_page_url( 'dienste-import' ) ?>" method="post">
         <?php 
             wp_nonce_field('dienste-spiele-importieren');
-            submit_button("Importieren"); 
+            submit_button("Spiele Importieren"); 
         ?>
     </form>
 </div>
@@ -63,5 +92,10 @@ function diensteImportSubmit(){
         $resultMessage .= "<b>$mannschaftsName</b>: ".$ergebnis->toReadableString()."<br>";
     }
     echo "<div style='margin-left:200px;'>$resultMessage</div>";
+}
+
+function meisterschaften_importieren(){
+    echo importMeisterschaftenFromNuliga();
+    exit;
 }
 ?>
