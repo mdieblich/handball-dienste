aba6b5d53573d97039fac6fbc8faad9acf630845
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-24 14:01:13 +0200

    Import der Spiele über Ajax

--- wordpress/menues/import.php
+++ wordpress/menues/import.php
@@ -2,10 +2,9 @@
 
 function addDiensteSpieleImportKonfiguration(){
     $hook_import = add_submenu_page( 'dienste', 'Dienste - Spiele importieren', 'Import', 'administrator', 'dienste-import', 'displaySpieleImport');
-    
-    add_action( 'load-' . $hook_import, 'diensteImportSubmit' );
 }
 add_action( 'wp_ajax_meisterschaften_importieren', 'meisterschaften_importieren' );
+add_action( 'wp_ajax_spiele_importieren', 'spiele_importieren' );
 
 function displaySpieleImport(){
     require_once __DIR__."/../dao/mannschaft.php";
@@ -13,13 +12,32 @@ function displaySpieleImport(){
     $mannschaften = loadMannschaftenMitMeisterschaften();
 ?>
 <script>
-function startImport(){
+function startImportMeisterschaften(){
     jQuery(function($){
         $("#import-result").hide();
         $("#loading-spinner").show(500);
     });
 
     var data = {'action': 'meisterschaften_importieren'};
+
+    // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+    jQuery.post(ajaxurl, data)
+        .done(function(response){    
+            jQuery(function($){
+                $("#loading-spinner").hide(500, function(){
+                    $("#import-result").show(500);
+                });
+                $("#import-result").html("<pre>" + response + "</pre>");
+            });
+        });
+}
+function startImportSpiele(){
+    jQuery(function($){
+        $("#import-result").hide();
+        $("#loading-spinner").show(500);
+    });
+
+    var data = {'action': 'spiele_importieren'};
 
     // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
     jQuery.post(ajaxurl, data)
@@ -63,37 +81,20 @@ function startImport(){
         </tr>
         <?php } ?>
     </table>
-    <button class="btn btn-primary" onclick="startImport()">Meisterschaften importieren</button>
-    <form action="<?php menu_page_url( 'dienste-import' ) ?>" method="post">
-        <?php 
-            wp_nonce_field('dienste-spiele-importieren');
-            submit_button("Spiele Importieren"); 
-        ?>
-    </form>
+    <button class="btn btn-primary" onclick="startImportMeisterschaften()">Meisterschaften importieren</button>
+    <button class="btn btn-primary" onclick="startImportSpiele()">Spiele importieren</button>
 </div>
  <?php
 }
 
-function diensteImportSubmit(){
-    if('POST' !== $_SERVER['REQUEST_METHOD']){
-        return;
-    }
-    if(empty($_POST['submit'])){
-        return;
-    }
-    if("Importieren" !== $_POST['submit']){
-        return;
-    }
-    if(!check_admin_referer('dienste-spiele-importieren')){
-        return;
-    }
+function spiele_importieren(){
     require_once __DIR__."/../import/importer.php";
     $importErgebnis = importSpieleFromNuliga();
-    $resultMessage = "";
+    echo "Ergebnis des Imports der Spiele:\n";
     foreach($importErgebnis as $mannschaftsName => $ergebnis){
-        $resultMessage .= "<b>$mannschaftsName</b>: ".$ergebnis->toReadableString()."<br>";
+        echo $mannschaftsName.": ".$ergebnis->toReadableString()."\n";
     }
-    echo "<div style='margin-left:200px;'>$resultMessage</div>";
+    exit;
 }
 
 function meisterschaften_importieren(){
