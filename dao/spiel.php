3353a8917b4e5cc50e6cb5ebd68f63d0eb12596a
Martin Dieblich <martin.dieblich@gmx.de>
2022-06-24 14:20:22 +0200

    einrückung

--- dao/spiel.php
+++ dao/spiel.php
@@ -31,44 +31,45 @@ function loadSpieleDeep(string $whereClause="anwurf > subdate(current_date, 1)",
 }
 
 function countSpiele(int $mannschaftsID): int {
-  global $wpdb;
-  $table_name = $wpdb->prefix . 'spiel';
-  return $wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE mannschaft=$mannschaftsID");
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'spiel';
+    return $wpdb->get_var("SELECT COUNT(*) FROM $table_name WHERE mannschaft=$mannschaftsID");
 }
 
 function findSpiel(int $spielnr, int $mannschaft_id, int $gegner_id, int $isHeimspiel): ?Spiel{
-  global $wpdb;
-  $table_name = $wpdb->prefix . 'spiel';
-  $result = $wpdb->get_row("SELECT * FROM $table_name WHERE spielnr=$spielnr AND mannschaft=$mannschaft_id AND gegner=$gegner_id AND heimspiel=$isHeimspiel", ARRAY_A);
-  if(empty($result)){
-    return null;
-  }
-  return new Spiel($result);
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'spiel';
+    $result = $wpdb->get_row("SELECT * FROM $table_name WHERE spielnr=$spielnr AND mannschaft=$mannschaft_id AND gegner=$gegner_id AND heimspiel=$isHeimspiel", ARRAY_A);
+    if(empty($result)){
+      return null;
+    }
+    return new Spiel($result);
 }
 
 function insertSpiel(int $spielnr, int $mannschaft_id, int $gegner_id, bool $isHeimspiel, int $halle, ?DateTime $anwurf){
-  global $wpdb;
-  
-  $table_name = $wpdb->prefix . 'spiel';
-  $wpdb->insert($table_name, array(
-    'spielnr' => $spielnr, 
-    'mannschaft' => $mannschaft_id, 
-    'gegner' => $gegner_id, 
-    'heimspiel' => $isHeimspiel, 
-    'halle' => $halle, 
-    'anwurf' => isset($anwurf) ? $anwurf->format('Y-m-d H:i:s') : null
-  ));
+    global $wpdb;
+    
+    $table_name = $wpdb->prefix . 'spiel';
+    $wpdb->insert($table_name, array(
+        'spielnr' => $spielnr, 
+        'mannschaft' => $mannschaft_id, 
+        'gegner' => $gegner_id, 
+        'heimspiel' => $isHeimspiel, 
+        'halle' => $halle, 
+        'anwurf' => isset($anwurf) ? $anwurf->format('Y-m-d H:i:s') : null
+    ));
 }
 function updateSpiel(int $id, int $halle, ?DateTime $anwurf){
-  global $wpdb;
-  
-  $table_name = $wpdb->prefix . 'spiel';
-  $wpdb->update($table_name, 
-    array(
-      'halle' => $halle, 
-      'anwurf' => isset($anwurf) ? $anwurf->format('Y-m-d H:i:s') : null
-    ), array(
-      'id' => $id
-    ));
+    global $wpdb;
+    
+    $table_name = $wpdb->prefix . 'spiel';
+    $wpdb->update($table_name, 
+        array(
+          'halle' => $halle, 
+          'anwurf' => isset($anwurf) ? $anwurf->format('Y-m-d H:i:s') : null
+        ), array(
+          'id' => $id
+        )
+    );
 }
 ?>
