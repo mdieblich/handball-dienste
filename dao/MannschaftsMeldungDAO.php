9089f5c6d47e1add7ef250cacd50178c09956a6e
Martin Dieblich <martin.dieblich@gmx.de>
2022-07-28 14:55:49 +0200

    überflüssige Methode entfernt

--- dao/MannschaftsMeldungDAO.php
+++ dao/MannschaftsMeldungDAO.php
@@ -3,10 +3,6 @@ require_once __DIR__."/DAO.php";
 require_once __DIR__."/../handball/MannschaftsMeldung.php";
 
 class MannschaftsMeldungDAO extends DAO{
-
-    public function loadMannschaftsMeldungen(string $where = null, string $orderBy = null): array{
-        return $this->fetchAll($where, $orderBy);
-    }
     
     public function findMannschaftsMeldung(int $meisterschaft, int $mannschaft, string $liga): ?MannschaftsMeldung {
         return $this->fetch("meisterschaft_id=$meisterschaft AND mannschaft_id=$mannschaft AND liga=\"$liga\"");
