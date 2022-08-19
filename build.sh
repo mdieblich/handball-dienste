75686ad134e4e838daf55a3e11c5e280f2b7a736
Martin Dieblich <martin.dieblich@gmx.de>
2022-08-20 00:42:03 +0200

    phpspreadsheet hinzugefügt

--- build.sh
+++ build.sh
@@ -19,6 +19,7 @@ cp -r ../zeit dienstedienst
 cp -r ../dao dienstedienst
 cp -r ../service dienstedienst
 cp -r ../components dienstedienst
+cp -r ../vendor dienstedienst
 cp -r ../wordpress/* dienstedienst
 
 # 3. Zippen
