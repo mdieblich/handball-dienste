897f7010472444402c98a329b998760042e40421
Martin Dieblich <martin.dieblich@gmx.de>
2022-08-26 22:42:43 +0200

    Klasse für Halle

---
+++ handball/Halle.php
@@ -0,0 +1,12 @@
+<?php
+
+class Halle {
+    public int $id;
+    public int $nummer;
+    public int $nuliga_id;
+    public string $bezeichnung;
+    public string $adresse_zeile_1;
+    public string $adresse_zeile_2;
+}
+
+?>
