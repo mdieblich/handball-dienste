17202605adcf25d997041adeb4fe81aba1b6a415
Martin Dieblich <martin.dieblich@gmx.de>
2022-08-29 00:22:45 +0200

    TeamID extrahieren achtet auf Mannschaftsnummer
    wichtig, falls mehrere Mannschaften in gleicher Liga

--- wordpress/import/meisterschaft/NuLiga_Ligatabelle.php
+++ wordpress/import/meisterschaft/NuLiga_Ligatabelle.php
@@ -12,10 +12,19 @@ class NuLiga_Ligatabelle {
         $this->xpath = new DOMXPath($this->dom);
     }
 
-    public function extractTeamID(string $vereinsname): ?int {
+    public function extractTeamID(string $vereinsname, int $mannschaftsNummer): ?int {
+        $gesuchteMannschaft = $vereinsname;
+        switch($mannschaftsNummer) {
+            case 1: break;
+            case 2: $gesuchteMannschaft .= " II"; break;
+            case 3: $gesuchteMannschaft .= " III"; break;
+            case 4: $gesuchteMannschaft .= " IV"; break;
+            case 5: $gesuchteMannschaft .= " V"; break;
+            default: $gesuchteMannschaft .= " ".$mannschaftsNummer; break;
+        }
         foreach($this->extractMannschaftsZellen() as $mannschaftsZelle){
             $mannschaftsName = sanitizeContent($mannschaftsZelle->textContent);
-            if(strpos($mannschaftsName, $vereinsname) === 0){
+            if($mannschaftsName === $gesuchteMannschaft){
                 return $this->extractTeamIDFromZelle($mannschaftsZelle);
             }
         }
