c623ad4056c4958e901975c6ba9fe18069189fe8
Martin Dieblich <martin.dieblich@gmx.de>
2022-09-11 02:35:28 +0200

    Laden aller Log-Dateien

--- log/Log.php
+++ log/Log.php
@@ -15,6 +15,17 @@ class Log {
     public function log(string $message){
         fwrite($this->fileHandle, $message."\n");
     }
+
+    public static function findFiles(string $purpose) : array {
+        $fileNames = array();
+        $path = plugin_dir_path(__FILE__);
+        $path_length = strlen($path);
+        foreach(glob("$path*-".$purpose.".txt") as $fileName){
+            $date = DateTime::createFromFormat("Y.m.d_H.i.s", substr($fileName, $path_length, 19));
+            $fileNames[$date->getTimestamp()] =  $fileName;
+        }
+        return $fileNames;
+    }
 }
 
 ?>
