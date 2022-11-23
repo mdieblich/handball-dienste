b1dafb6d6f56fd442cf6970146c8d679f5b1e075
Martin Dieblich <martin.dieblich@gmx.de>
2022-11-23 22:38:38 +0100

    VereinDAO erstellt

---
+++ dao/VereinDAO.php
@@ -0,0 +1,9 @@
+<?php
+require_once __dir__."/../handball/Gegner.php";
+require_once __dir__."/../handball/MannschaftsMeldung.php";
+require_once __DIR__."/DAO.php";
+
+class VereinDAO extends DAO{
+    
+}
+?>
