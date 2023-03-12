8279c67da4e70dfcf24597bcfedb6b8e75dbda4e
Martin Dieblich <martin.dieblich@gmx.de>
2023-03-12 23:31:09 +0100

    Test von getAenderung() führt zu fließenderem Text

--- handball/dienst/SpielAenderung.php
+++ handball/dienst/SpielAenderung.php
@@ -12,13 +12,19 @@ class SpielAenderung{
 
     public function getAenderung(): string{
         $message = "";
-        if($this->alt->anwurf != $this->neu->anwurf){
+        $anwurfDiffers = $this->alt->anwurfDiffers($this->neu);
+        $halleDiffers = $this->alt->halleDiffers($this->neu);
+
+        if($anwurfDiffers){
             $message .= "Anwurf "
                 ."von [".$this->alt->anwurf->format("d.m.Y H:i")."]"
                 ." zu [".$this->neu->anwurf->format("d.m.Y H:i")."]";
         }
-        if($this->alt->halle != $this->neu->halle){
-            $message .= " Halle "
+        if($anwurfDiffers && $halleDiffers){
+            $message .= " und ";
+        }
+        if($halleDiffers){
+            $message .= "Halle "
                 ."von [".$this->alt->halle."]"
                 ." zu [".$this->neu->halle."]";
         }
