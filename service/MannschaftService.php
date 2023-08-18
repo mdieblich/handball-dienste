4222d365730030bdf442d07f32a508b3658958f2
Martin Dieblich <martin.dieblich@gmx.de>
2023-08-19 00:10:19 +0200

    Laden von Meldung mit Service ausgelagert

--- service/MannschaftService.php
+++ service/MannschaftService.php
@@ -1,19 +1,17 @@
 <?php
 
 require_once __dir__."/../handball/MannschaftsListe.php";
+
+require_once __dir__."/MannschaftsMeldungService.php";
 require_once __dir__."/../dao/MannschaftDAO.php";
-require_once __dir__."/../dao/MannschaftsMeldungDAO.php";
-require_once __dir__."/../dao/MeisterschaftDAO.php";
 
 class MannschaftService{
     private MannschaftDAO $mannschaftDAO;
-    private MannschaftsMeldungDAO $meldungDAO;
-    private MeisterschaftDAO $meisterschaftDAO;
+    private MannschaftsMeldungService $meldungService;
 
     public function __construct($dbhandle=null){
         $this->mannschaftDAO = new MannschaftDAO($dbhandle);
-        $this->meldungDAO = new MannschaftsMeldungDAO($dbhandle);
-        $this->meisterschaftDAO = new MeisterschaftDAO($dbhandle);
+        $this->meldungService = new MannschaftsMeldungService($dbhandle);
     }
     
     public function loadMannschaftenMitMeldungen(): MannschaftsListe{
@@ -23,11 +21,9 @@ class MannschaftService{
             return $mannschaftsListe;
         }
         
-        $meisterschaften = $this->meisterschaftDAO->loadMeisterschaften();
-
         $mannschaftIDs = $mannschaftsListe->getIDs();
         $filter = "mannschaft_id in (".implode(", ", $mannschaftIDs).")";
-        $meldungen = $this->meldungDAO->fetchAll($filter);
+        $meldungen = $this->meldungService->loadMannschaftsMeldungenMitMeisterschaften($filter);
 
         foreach($meldungen as $meldung){
             // Mannschaft zuweisen
@@ -35,11 +31,6 @@ class MannschaftService{
             $mannschaft->meldungen[$meldung->id] = $meldung;
             $meldung->mannschaft = $mannschaft;
             unset($meldung->mannschaft_id);
-
-            // Meisterschaft zuweisen
-            $meisterschaft = $meisterschaften[$meldung->meisterschaft_id];
-            $meldung->meisterschaft = $meisterschaft;
-            unset($meldung->meisterschaft_id);
         }
 
         return $mannschaftsListe;
---
+++ service/MannschaftsMeldungService.php
@@ -0,0 +1,30 @@
+<?php
+require_once __dir__."/../dao/MannschaftsMeldungDAO.php";
+require_once __dir__."/../dao/MeisterschaftDAO.php";
+
+class MannschaftsMeldungService {
+    private MannschaftsMeldungDAO $meldungDAO;
+    private MeisterschaftDAO $meisterschaftDAO;
+
+    public function __construct($dbhandle=null){
+        $this->meldungDAO = new MannschaftsMeldungDAO($dbhandle);
+        $this->meisterschaftDAO = new MeisterschaftDAO($dbhandle);
+    }
+
+    public function loadMannschaftsMeldungenMitMeisterschaften(strign $where=null): array{
+        $meldungen = $this->meldungDAO->fetchAll($where);
+        if(empty($meldungen)){
+            return $meldungen;
+        }
+        
+        $meisterschaften = $this->meisterschaftDAO->loadMeisterschaften();
+        foreach($meldungen as $meldung){
+            $meisterschaft = $meisterschaften[$meldung->meisterschaft_id];
+            $meldung->meisterschaft = $meisterschaft;
+            unset($meldung->meisterschaft_id);
+        }
+
+        return $meldungen;
+    }
+}
+?>
