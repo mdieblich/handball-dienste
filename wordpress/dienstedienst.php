408ae83a178c832ca94c948c451512653906ab44
Martin Dieblich <martin.dieblich@gmx.de>
2023-08-18 22:45:28 +0200

    Version 1.18.5
    hochgezählt wegen Wechsel zu HNR

--- wordpress/dienstedienst.php
+++ wordpress/dienstedienst.php
@@ -2,7 +2,7 @@
  /*
  Plugin Name: Dienstedienst
  Description: Plugin zum Verwalten zusätzlicher Dienste (Zeitnehmer, Catering...) beim Handball
- Version: 1.18.4
+ Version: 1.18.5
  Author: Martin Dieblich
  Author URI: https://www.turnerkreisnippes.de
  */
