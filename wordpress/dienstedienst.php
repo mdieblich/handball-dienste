c844ca89b096e01c5fa9d4d701c7ad5f5a4c6bbe
Martin Dieblich <martin.dieblich@gmx.de>
2023-08-23 21:05:01 +0200

    falsche Iterationsreihenfolge für Spiel-Zellen

--- wordpress/dienstedienst.php
+++ wordpress/dienstedienst.php
@@ -2,7 +2,7 @@
  /*
  Plugin Name: Dienstedienst
  Description: Plugin zum Verwalten zusätzlicher Dienste (Zeitnehmer, Catering...) beim Handball
- Version: 1.18.7
+ Version: 1.18.8
  Author: Martin Fritz
  Author URI: https://www.turnerkreisnippes.de
  */
