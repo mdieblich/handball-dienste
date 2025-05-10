02d0985c71c6cb8be635e755bd1135b553634840
Martin Dieblich <martin.dieblich@gmx.de>
2025-05-10 13:59:43 +0200

    BugFix: Keine Objketinitisierung als Funktionsparameter

--- wordpress/dienstedienst.php
+++ wordpress/dienstedienst.php
@@ -2,7 +2,7 @@
  /*
  Plugin Name: Dienstedienst
  Description: Plugin zum Verwalten zusätzlicher Dienste (Zeitnehmer, Catering...) beim Handball
- Version: 1.19.0
+ Version: 1.19.1
  Author: Martin Fritz
  Author URI: https://www.turnerkreisnippes.de
  */
