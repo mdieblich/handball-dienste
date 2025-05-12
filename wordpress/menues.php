7d133ebf933fc361898d0eabeb6e69d004dde29e
Martin Fritz <martin@fritz.koeln>
2025-05-12 10:22:05 +0200

    Ersten Menüeintrag umbenannt

--- wordpress/menues.php
+++ wordpress/menues.php
@@ -6,15 +6,15 @@ require_once WP_PLUGIN_DIR."/dienstedienst/menues/zuweisen.php";
 
 function addDiensteMenueeintraege() {
     //add_menu_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-    add_menu_page(  'Dienste', 'Dienste', 'administrator', 'dienste', 'displayDiensteDashboard', 'dashicons-schedule' );
-    
+    add_menu_page(  'Dienste-Plugin', 'Dienste', 'administrator', 'dienste', 'displayDiensteDashboard', 'dashicons-schedule' );
     //add_submenu_page( '$parent_slug, $page_title, $menu_title, $capability, $menu_slug, $function );
+    add_submenu_page( 'dienste', 'Anleitung für Dienste-Plugin', 'Anleitung', 'administrator', 'dienste', 'displayDiensteDashboard');
+    
     addDiensteMannschaftsKonfiguration();
     addDiensteSpieleImportKonfiguration();
     addDiensteGegnerKonfiguration();
     addDiensteZuweisenKonfiguration();
 }
-
 
 function displayDiensteDashboard(){
     include __DIR__."/anleitung.php";
