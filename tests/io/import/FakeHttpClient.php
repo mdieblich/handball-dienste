9299841b851a8f1b071d8e372796353e9fc3618d
Martin Fritz <martin@fritz.koeln>
2025-06-19 22:34:19 +0200

    FakeHttpClient

---
+++ tests/io/import/FakeHttpClient.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../../../../src/io/import/HttpClient.php';
+
+class FakeHttpClient implements HttpClient {
+
+    private array $data = [];
+
+    public function set(string $url, string $data): void {
+        $this->data[$url] = $data;
+    }
+    
+    public function fetch(string $url): string {
+        return $this->data[$url] ??'';
+    }
+
+}
