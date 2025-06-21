74a55a9293132d94d2dfa5960c07cfa067ecae3b
Martin Fritz <martin@fritz.koeln>
2025-06-21 20:10:18 +0200

    Test für das Laden mehrerer Mannschaften ergänzt

--- tests/io/import/SpieleImportTest.php
+++ tests/io/import/SpieleImportTest.php
@@ -54,7 +54,7 @@ final class SpieleImportTest extends TestCase {
         // act
         $files = $this->import->fetchAllNuligaSpielelisten();
 
-        //
+        // assert
         $this->assertCount(1, $files);
         $this->assertEquals("<html>Example-HTML</html>", file_get_contents($files[0]), "Die gespeicherte HTML-Datei stimmt nicht mit der erwarteten überein.");
     }
@@ -107,10 +107,64 @@ final class SpieleImportTest extends TestCase {
         // act
         $files = $this->import->fetchAllNuligaSpielelisten();
         
-        //
+        // assert
         $this->assertCount(2, $files);
         $this->assertEquals("<html>Example-HTML 1</html>", file_get_contents($files[0]), "Die gespeicherte HTML-Datei stimmt nicht mit der erwarteten überein.");
         $this->assertEquals("<html>Example-HTML 2</html>", file_get_contents($files[1]), "Die gespeicherte HTML-Datei stimmt nicht mit der erwarteten überein.");
     }
-    // TODO: Test für mehrere Mannschaften
+    
+
+    public function test_fetchAllNuligaSpielelisten_laedtMehrereMannschaften() {
+        // arrange
+        $meisterschaft = "KR 24/25"; // Köln/Rheinberg 2024/25
+        $gruppe1 = 363515;   // Regionsliga Männer
+        $team_id1 = 1986866; // Turnerkreis Nippes 2 (Herren)
+
+        $gruppe2 = 363729;   // Regionsklasse Männer
+        $team_id2 = 1986887; // Turnerkreis Nippes 3 (Herren)
+
+        $meisterschaft_id = $this->builder->createMeisterschaft($meisterschaft);
+        $mannschaft_id1 = $this->builder->createMannschaft(2);
+        $this->builder->createMannschaftsMeldung(
+            $mannschaft_id1,
+            $meisterschaft_id,
+            $gruppe1,
+            $team_id1
+        );
+
+        $this->httpClient->set(
+            NuLiga_SpiellisteTeam::$BASE_URL
+                ."teamtable=$team_id1&"
+                ."pageState=vorrunde&"
+                ."championship=".urlencode($meisterschaft)."&"
+                ."group=$gruppe1",
+            "<html>Example-HTML 1</html>"
+        );
+
+        $mannschaft_id2 = $this->builder->createMannschaft(3);
+        $this->builder->createMannschaftsMeldung(
+            $mannschaft_id2,
+            $meisterschaft_id,
+            $gruppe2,
+            $team_id2
+        );
+
+        $this->httpClient->set(
+            NuLiga_SpiellisteTeam::$BASE_URL
+                ."teamtable=$team_id2&"
+                ."pageState=vorrunde&"
+                ."championship=".urlencode($meisterschaft)."&"
+                ."group=$gruppe2",
+            "<html>Example-HTML 2</html>"
+        );
+
+
+        // act
+        $files = $this->import->fetchAllNuligaSpielelisten();
+
+        // assert
+        $this->assertCount(2, $files);
+        $this->assertEquals("<html>Example-HTML 1</html>", file_get_contents($files[0]), "Die gespeicherte HTML-Datei stimmt nicht mit der erwarteten überein.");
+        $this->assertEquals("<html>Example-HTML 2</html>", file_get_contents($files[1]), "Die gespeicherte HTML-Datei stimmt nicht mit der erwarteten überein.");
+    }
 }
