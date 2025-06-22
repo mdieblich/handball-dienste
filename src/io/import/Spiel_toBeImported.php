65ffbd5b76720d2837525424d909a8a99cb316f2
Martin Fritz <martin@fritz.koeln>
2025-06-22 21:38:25 +0200

    Zu importierende Spiele als separate Klasse

--- src/io/import/Spiel_toBeImported.php
+++ src/io/import/Spiel_toBeImported.php
@@ -2,4 +2,20 @@
 
 require_once __DIR__."/../../handball/Spiel.php";
 
-class Spiel_toBeImported extends Spiel{}
+class Spiel_toBeImported{
+    
+    public int $id;
+    public int $spielNr;
+    public int $meldung_id;
+    public string $gegnerName;
+    public ?DateTime $anwurf = null;
+    public int $halle;
+    public bool $heimspiel;
+    public DateTime $importDatum;
+
+    public function __construct(){
+        $this->importDatum = new DateTime();
+    }
+
+    // TODO Umwandlung in ein Spiel-Objekt
+}
