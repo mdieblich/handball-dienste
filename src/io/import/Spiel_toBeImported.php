210ebf7f15cf653e3840af795bdfd4a595ad9434
Martin Fritz <martin@fritz.koeln>
2025-06-24 10:32:06 +0200

    Member ergänzt, um Suche nach vorhandenen Spielen abzubilden

--- src/io/import/Spiel_toBeImported.php
+++ src/io/import/Spiel_toBeImported.php
@@ -3,17 +3,21 @@
 require_once __DIR__."/../../handball/Spiel.php";
 
 class Spiel_toBeImported{
+    public DateTime $importDatum;
     
     public int $id;
     public int $spielNr;
     public int $meldung_id;
     public string $gegnerName;
-    public ?int $gegner_id; // ID des Gegners in der Datenbank, nachdem er gefunden wurde
     public ?DateTime $anwurf = null;
     public string $halle;
     public bool $heimspiel;
-    public DateTime $importDatum;
 
+    public ?int $gegner_id; // ID des Gegners in der Datenbank, nachdem er gefunden wurde
+
+    public ?bool $istNeuesSpiel = null; // Flag, ob es sich um ein neues Spiel handelt, das noch nicht in der Datenbank ist
+    public ?int $spielID_alt = null; // ID des alten Spiels, falls es aktualisiert wird
+    
     public function __construct(){
         $this->importDatum = new DateTime();
     }
