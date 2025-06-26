228ec4dd2d90e6bb9f7b6f434bba37664d95d757
Martin Fritz <martin@fritz.koeln>
2025-06-26 20:16:50 +0200

    DienstAenderung aufgrund eines bestehenden Spieles erstellen

--- src/io/import/DienstAenderung.php
+++ src/io/import/DienstAenderung.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__."/../../handball/Spiel.php";
+
 class DienstAenderung {
     public int $id;
     public int $dienstID;
@@ -9,5 +11,13 @@ class DienstAenderung {
 
     public ?DateTime $anwurfVorher = null;
     public ?string $halleVorher = null;
+
+    public static function create(int $dienstID, Spiel $spiel_vorher): DienstAenderung {
+        $aenderung = new DienstAenderung();
+        $aenderung->dienstID = $dienstID;
+        $aenderung->anwurfVorher = $spiel_vorher->anwurf;
+        $aenderung->halleVorher = $spiel_vorher->halle;
+        return $aenderung;
+    }
 
 }
