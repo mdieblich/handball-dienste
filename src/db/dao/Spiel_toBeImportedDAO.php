7ad24c714326a8940edfa29f8d77e1315b4f708d
Martin Fritz <martin@fritz.koeln>
2025-06-26 13:52:41 +0200

    holen aller SPiele, die geupdated werden können

--- src/db/dao/Spiel_toBeImportedDAO.php
+++ src/db/dao/Spiel_toBeImportedDAO.php
@@ -4,4 +4,8 @@ require_once __DIR__."/../../io/import/Spiel_toBeImported.php";
 require_once __DIR__."/DAO.php";
 
 class Spiel_toBeImportedDAO extends DAO{
+
+    public function fetchAllForUpdate(): array{
+        return $this->fetchAll("gegner_id is not null and spielID_alt is not null");
+    }
 }
