9d464d266c3eb264ac0613f01f30ca8919761667
Martin Fritz <martin@fritz.koeln>
2025-06-30 20:55:26 +0200

    Anwurf optional

--- tests/db/DBBuilder.php
+++ tests/db/DBBuilder.php
@@ -40,12 +40,12 @@ class DBBuilder{
         return $this->db->insert_id;
     }
 
-    public function createSpiel(int $spielNr, int $meldung_id, int $gegner_id, DateTime $anwurf, string $halle, bool $heimspiel): int {
+    public function createSpiel(int $spielNr, int $meldung_id, int $gegner_id, ?DateTime $anwurf, string $halle, bool $heimspiel): int {
         $this->db->insert("wp_spiel", [
             "spielNr" => $spielNr,
             "mannschaftsMeldung_id" => $meldung_id,
             "gegner_id" => $gegner_id,
-            "anwurf" => $anwurf->format('Y-m-d H:i:s'),
+            "anwurf" => $anwurf?->format('Y-m-d H:i:s'),
             "halle" => $halle,
             "heimspiel" => $heimspiel
         ]);
