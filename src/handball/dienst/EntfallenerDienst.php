dd521d450750285d113723a3cf87cf735c526c34
Martin Fritz <martin@fritz.koeln>
2025-06-30 23:37:01 +0200

    id ergänzt

--- src/handball/dienst/EntfallenerDienst.php
+++ src/handball/dienst/EntfallenerDienst.php
@@ -4,6 +4,7 @@ require_once __DIR__."/../Spiel.php";
 require_once __DIR__."/../Mannschaft.php";
 
 class EntfallenerDienst {
+    public int $id;
     public Spiel $spiel;            public int $spiel_id;
     public string $dienstart;
     public ?Mannschaft $mannschaft; public ?int $mannschaft_id;
