f4b0b3f42efbd6ceb491fdd6cecabdc1c3525aa1
Martin Fritz <martin@fritz.koeln>
2025-06-30 23:37:17 +0200

    NeuerDienst als Klasse erstellt (mit DAO)

---
+++ src/db/dao/dienst/NeuerDienstDAO.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once __DIR__."/../../../handball/Dienst/NeuerDienst.php";
+require_once __DIR__."/../DAO.php";
+
+class NeuerDienstDAO extends DAO{
+}
---
+++ src/handball/dienst/NeuerDienst.php
@@ -0,0 +1,7 @@
+<?php
+
+class NeuerDienst {
+    public int $id;
+    public int $dienst_id;
+    public string $grund;
+}
