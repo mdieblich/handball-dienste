a572f6cf242db74247f8859f05d008c910c6d309
Martin Fritz <martin@fritz.koeln>
2025-07-04 01:17:26 +0200

    EntfallenerDienstService

---
+++ src/db/service/EntfallenerDienstSevice.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once __dir__."/../dao/dienst/EntfallenerDienstDAO.php";
+require_once __dir__."/../dao/DienstDAO.php";
+
+class EntfallenerDienstService{
+    private DienstDAO $dienstDAO;
+    private EntfallenerDienstDAO $entfallenerDienstDAO;
+
+    public function __construct($dbhandle){
+        $this->dienstDAO = new DienstDAO($dbhandle);
+        $this->entfallenerDienstDAO = new EntfallenerDienstDAO($dbhandle);
+    }
+
+    public function replaceAll(array $entfalleneDienste): void{
+        foreach($entfalleneDienste as $entfallenerDienst){
+            $this->entfallenerDienstDAO->insert($entfallenerDienst);
+            $this->dienstDAO->delete(['id' => $entfallenerDienst->dienst->id]);
+        }
+    }
+}
