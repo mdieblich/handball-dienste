11555eb31b35f779c52dd528750554f86cb64997
Martin Fritz <martin@fritz.koeln>
2025-07-03 16:26:38 +0200

    Konstruktor mit Dienst + Grund

--- src/handball/dienst/NeuerDienst.php
+++ src/handball/dienst/NeuerDienst.php
@@ -5,4 +5,9 @@ class NeuerDienst {
     public int $id;
     public Dienst $dienst; public int $dienst_id;
     public string $grund;
+
+    public function __construct(Dienst $dienst, string $grund) {
+        $this->dienst = $dienst;
+        $this->grund = $grund;
+    }
 }
