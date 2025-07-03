64eb6288c0ef31f82427ba719aa344a4d8df66e2
Martin Fritz <martin@fritz.koeln>
2025-07-03 16:13:02 +0200

    Konstruktor, der Daten vom Dienst kopiert

--- src/handball/dienst/EntfallenerDienst.php
+++ src/handball/dienst/EntfallenerDienst.php
@@ -11,4 +11,17 @@ class EntfallenerDienst {
     public string $dienstart;
     public ?Mannschaft $mannschaft; public ?int $mannschaft_id;
     public string $grund;
+
+    public function __construct(Dienst $dienst, string $grund){
+        $this->dienst = $dienst;
+        $this->dienst_id = $dienst->id;
+
+        $this->spiel = $dienst->spiel;
+        $this->spiel_id = $dienst->spiel_id;
+        
+        $this->mannschaft = $dienst->mannschaft;
+        $this->mannschaft_id = $dienst->mannschaft_id;
+
+        $this->grund = $grund;
+    }
 }
