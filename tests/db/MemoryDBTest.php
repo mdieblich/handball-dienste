0dbff84823065754fc566acfa6a9866c0c8eba89
Martin Fritz <martin@fritz.koeln>
2025-07-05 12:33:44 +0200

    test von select in

--- tests/db/MemoryDBTest.php
+++ tests/db/MemoryDBTest.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__."/../../db/MemoryDB.php";
+require_once __DIR__."/MemoryDB.php";
 
 final class MemoryDBTest extends TestCase {
 
@@ -11,42 +11,82 @@ final class MemoryDBTest extends TestCase {
         $this->db = new MemoryDB();
     }
 
-    public function test_subselect() {
+    public function test_select_in() {
         // arrange
-        $herren1_id = $this->db->insert("mannschaft", [
+        $this->db->insert("mannschaft", [
             'name'       => 'Herren I',
             'nummer'     => 1,
             'geschlecht' => 'm'
         ]);
-        $herren2_id = $this->db->insert("mannschaft", [
+        $herren1_id = $this->db->insert_id;
+        $this->db->insert("mannschaft", [
             'name'       => 'Herren II',
             'nummer'     => 2,
             'geschlecht' => 'm'
         ]);
-        $damen1_id = $this->db->insert("mannschaft", [
+        $herren2_id = $this->db->insert_id;
+        $this->db->insert("mannschaft", [
             'name'       => 'Damen I',
             'nummer'     => 1,
             'geschlecht' => 'w'
         ]);
-        $damen2_id = $this->db->insert("mannschaft", [
+        $damen1_id = $this->db->insert_id;
+        $this->db->insert("mannschaft", [
             'name'       => 'Damen II',
             'nummer'     => 2,
             'geschlecht' => 'w'
         ]);
+        $damen2_id = $this->db->insert_id;
+        // act
+        $results = $this->db->get_results("SELECT * FROM mannschaft WHERE id IN ($herren1_id, $damen2_id))", ARRAY_A);
 
-        $meldung_h1_id = $this->db->insert('meldung', [
+        // assert
+        $this->assertCount(2, $results);
+        $ligen = array_column($results, 'name');
+        $this->assertContains( 'Herren I', $ligen);
+        $this->assertContains( 'Damen II', $ligen);
+    }
+    public function test_subselect() {
+        // arrange
+        
+        $this->db->insert("mannschaft", [
+            'name'       => 'Herren I',
+            'nummer'     => 1,
+            'geschlecht' => 'm'
+        ]);
+        $herren1_id = $this->db->insert_id;
+        $this->db->insert("mannschaft", [
+            'name'       => 'Herren II',
+            'nummer'     => 2,
+            'geschlecht' => 'm'
+        ]);
+        $herren2_id = $this->db->insert_id;
+        $this->db->insert("mannschaft", [
+            'name'       => 'Damen I',
+            'nummer'     => 1,
+            'geschlecht' => 'w'
+        ]);
+        $damen1_id = $this->db->insert_id;
+        $this->db->insert("mannschaft", [
+            'name'       => 'Damen II',
+            'nummer'     => 2,
+            'geschlecht' => 'w'
+        ]);
+        $damen2_id = $this->db->insert_id;
+
+        $this->db->insert('meldung', [
             'mannschaft' => $herren1_id,
             'liga'       => 'Oberliga Männer'
         ]);
-        $meldung_h2_id = $this->db->insert('meldung', [
+        $this->db->insert('meldung', [
             'mannschaft' => $herren2_id,
             'liga'       => 'Kreisliga Männer'
         ]);
-        $meldung_d1_id = $this->db->insert('meldung', [
+        $this->db->insert('meldung', [
             'mannschaft' => $damen1_id,
             'liga'       => 'Oberliga Frauen'
         ]);
-        $meldung_d2_id = $this->db->insert('meldung', [
+        $this->db->insert('meldung', [
             'mannschaft' => $damen2_id,
             'liga'       => 'Kreisliga Frauen'
         ]);
