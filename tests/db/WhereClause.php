1406a39bd55733e3a4d3a7b4e688db5bca2c4df1
Martin Fritz <martin@fritz.koeln>
2025-07-07 12:45:38 +0200

    Closure statt callable für abwärtskompaitibilität <PHP 8.2

--- tests/db/WhereClause.php
+++ tests/db/WhereClause.php
@@ -2,8 +2,8 @@
 
 class WhereClause {
     private string $where;
-    private ?callable $subselect_resolver;
-    public function __construct(string $where, callable $subselect_resolver = null ) {
+    private ?Closure $subselect_resolver;
+    public function __construct(string $where, Closure $subselect_resolver = null ) {
         $this->where = $where;
         $this->subselect_resolver = $subselect_resolver;
     }
