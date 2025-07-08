1c6521e1d5eb6dbd257f97a1648e85b71c9efaa1
Martin Fritz <martin@fritz.koeln>
2025-07-08 14:44:12 +0200

    Erste Implementierung von parse

--- tests/db/WhereClause.php
+++ tests/db/WhereClause.php
@@ -7,25 +7,72 @@ class WhereClause {
     private ?array $exactConditions;
     private ?array $setConditions;
     private ?array $nullChecks;
-    public function __construct(string $where, Closure $subselect_resolver = null ) {
+    public function __construct(string $where, Closure $subselect_resolver = null) {
         $this->where = $where;
         $this->subselect_resolver = $subselect_resolver;
     }
 
     private function parse(): void {
+        $whereClauseParts = preg_split('/ AND /i', $this->where);
+        $this->exactConditions = [];      // "id=3" oder "aktiv=1" oder "spielOld is null"
+        $this->setConditions = [];        // "id in (1,2,3)"
+        $this->nullChecks = [];       // "anwurf is not null"
+        // TODO "Ungleich"-Bedingungen "íd != 5"
+        foreach($whereClauseParts as $whereClausePart){
+            $whereClausePart_lowerCase = strtolower($whereClausePart);
+            if(str_contains($whereClausePart,'=')){
+                $keyAndValue = explode('=', $whereClausePart, 2);
+                if(count($keyAndValue) != 2){
+                    throw new Exception("FEHLER: Bedingung $whereClausePart fehlerhaft");
+                }
+                $key = trim($keyAndValue[0]);
+                $value = trim($keyAndValue[1]);
+                $this->exactConditions[$key] = $value;
+            } else if (str_contains($whereClausePart_lowerCase,"in")){
+                $keyAndValues = explode('in', $whereClausePart, 2);
+                if(count($keyAndValues) != 2){
+                    throw new Exception("FEHLER: Bedingung $whereClausePart fehlerhaft");
+                }
+                $key = trim($keyAndValues[0]);
+                $values = trim($keyAndValues[1]);
+                if(!str_starts_with($values, '(') || !str_starts_with($values,')')){
+                    throw new Exception("FEHLER: rechter Teil der Bedingung von $whereClausePart muss in runden Klammern sein");
+                }
+                $values = substr($values,1, -1);
+                // subselects erst abfrühstücken
+                if(str_starts_with(strtolower($values), 'select')){
+                    $subselect = $values;
+                    if(!$this->subselect_resolver){
+                        throw new Exception("FEHLER: Subselect vorhanden ($subselect), aber kein Subselect-Resolver im Konstruktor gesetzt");
+                    }
+                    $subselect_result = $this->subselect_resolver->call($this, $subselect, ARRAY_A);
+                    $valueArray = array_column($subselect_result,0);
+                } else {
+                    $valueArray = explode(',', $values);
+                }
+                $this->setConditions[$key] = $valueArray;
+            } else if (preg_match('/(\w*) is( not)? null/i', $whereClausePart, $whereClausePartMatches)){
+                $key = $whereClausePartMatches[1];
+                $checkIsNull = !isset($whereClausePartMatches[2]);
+                $this->nullChecks[$key] = $checkIsNull;
+            } else {
+                // $this->logfile->log("WARNUNG: Teil der Where-Clause nicht unterstützt - wird ignoriert: $whereClausePart");
+            }
+        }
+
 
     }
 
     public function getExactConditions(): array {
-        if($this->exactConditions == null) { $this->parse(); }
+        if(!isset($this->exactConditions)) { $this->parse(); }
         return $this->exactConditions;
     }
     public function getSetConditions(): array {
-        if($this->setConditions == null) { $this->parse(); }
+        if(!isset($this->setConditions)) { $this->parse(); }
         return $this->setConditions;
     }
     public function getNullChecks(): array {
-        if($this->nullChecks == null) { $this->parse(); }
+        if(!isset($this->nullChecks)) { $this->parse(); }
         return $this->nullChecks;
     }
 }
