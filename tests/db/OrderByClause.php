1b0f04e7acfc8fa7c5224f7cca443f5361975f86
Martin Fritz <martin@fritz.koeln>
2025-07-09 20:25:37 +0200

    parse implementiert

--- tests/db/OrderByClause.php
+++ tests/db/OrderByClause.php
@@ -6,12 +6,34 @@ class OrderByClause {
     public const DESC = "DESC";
     
     private string $orderBy;
+    private ?array $orderKeys;
 
     public function __construct(string $orderBy) {
         $this->orderBy = $orderBy;
     }
 
+    private function parse(): void {
+        $this->orderKeys = [];
+        $orderByParts = preg_split("/,/", $this->orderBy);
+        foreach($orderByParts as $orderByPart){
+            $orderByPart = trim($orderByPart);
+            $orderByPart_lowerCase = strtolower($orderByPart);
+            if(str_ends_with($orderByPart_lowerCase, "asc")){
+                $columnName = trim(substr($orderByPart, 0, -3));
+                $this->orderKeys[] = [$columnName, OrderByClause::ASC];
+            } else if (str_ends_with($orderByPart_lowerCase, "desc")){
+                $columnName = trim(substr($orderByPart, 0, -4));
+                $this->orderKeys[] = [$columnName, OrderByClause::DESC];
+            } else {
+                throw new Exception ("Order BY-ANweisungen müssen mit ASC oder DESC enden");
+            }
+        }
+    }
+
     public function getOrderKeys(): array {
-        return [];
+        if(!isset($this->orderKeys) ) {
+            $this->parse();
+        }
+        return $this->orderKeys;
     }
 }
