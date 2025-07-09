8c48a214f8fc66aed47b4fce27610d183ba724bc
Martin Fritz <martin@fritz.koeln>
2025-07-09 10:55:15 +0200

    Tests implementiert, die das parsen prüfen

--- tests/db/OrderByClause.php
+++ tests/db/OrderByClause.php
@@ -1,10 +1,17 @@
 <?php
 
 class OrderByClause {
+
+    public const ASC = "ASC";
+    public const DESC = "DESC";
     
     private string $orderBy;
 
     public function __construct(string $orderBy) {
         $this->orderBy = $orderBy;
+    }
+
+    public function getOrderKeys(): array {
+        return [];
     }
 }
--- tests/db/OrderByClauseTest.php
+++ tests/db/OrderByClauseTest.php
@@ -1,14 +1,36 @@
 <?php declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
-require_once __DIR__."/WhereClause.php";
+require_once __DIR__."/OrderByClause.php";
 
 final class OrderByClauseTest extends TestCase {
 
-    public function test_detectsOneASC() {$this->fail("Not implemented");}
-    public function test_detectsOneDESC() {$this->fail("Not implemented");}
-    public function test_detectsASCandDESC() {$this->fail("Not implemented");}
-    public function test_detectsDESCandASC() {$this->fail("Not implemented");}
+    public function test_detectsOneASC() {
+        $orderBy = new OrderByClause("id ASC");
+        $this->assertEquals([
+            ['id', OrderByClause::ASC]
+        ], $orderBy->getOrderKeys());
+    }
+    public function test_detectsOneDESC() {
+        $orderBy = new OrderByClause("id DESC");
+        $this->assertEquals([
+            ['id', OrderByClause::DESC]
+        ], $orderBy->getOrderKeys());
+    }
+    public function test_detectsASCandDESC() {    
+        $orderBy = new OrderByClause("id ASC, name DESC");
+        $this->assertEquals([
+            ['id', OrderByClause::ASC],
+            ['name', OrderByClause::DESC]
+        ], $orderBy->getOrderKeys());
+    }
+    public function test_detectsDESCandASC() {   
+        $orderBy = new OrderByClause("name DESC, id ASC");
+        $this->assertEquals([
+            ['name', OrderByClause::DESC],
+            ['id', OrderByClause::ASC]
+        ], $orderBy->getOrderKeys());
+    }
     public function test_sort_oneASC() {$this->fail("Not implemented");}
     public function test_sort_twoASC() {$this->fail("Not implemented");}
     public function test_sort_oneDESC() {$this->fail("Not implemented");}
