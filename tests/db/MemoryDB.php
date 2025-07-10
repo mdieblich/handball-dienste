ab521ec2fe0c6364e3316262b587ee1c8fbba6a7
Martin Fritz <martin@fritz.koeln>
2025-07-10 22:36:49 +0200

    WHERE-clause nihct greedy

--- tests/db/MemoryDB.php
+++ tests/db/MemoryDB.php
@@ -74,7 +74,7 @@ class MemoryDB {
     }
     
     public function get_results($query, $output = OBJECT): array {
-        if (!preg_match('/SELECT\s+((?:(?! FROM ).)+) FROM (\w+) WHERE (.+?)(?: ORDER BY (.*))?$/i', $query, $matches)) {
+        if (!preg_match('/SELECT\s+((?:(?! FROM ).)+) FROM (\w+)(?: WHERE (.+?))?(?: ORDER BY (.*))?$/i', $query, $matches)) {
             $this->logfile->log("FEHLER: Query passt nicht zu Format: $query");
             return [];
         }
