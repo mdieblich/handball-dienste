f7c329ba3573630ffb571fa0737ac1aaf2f885b6
Martin Fritz <martin@fritz.koeln>
2025-07-11 21:36:00 +0200

    Erst prüfen, ob Dienst überhaupt einer Mannschaft zugeordet ist

--- src/handball/dienst/DienstAenderung.php
+++ src/handball/dienst/DienstAenderung.php
@@ -7,14 +7,16 @@ require_once __DIR__."/../Mannschaft.php";
 class DienstAenderung {
     public int $id;
     public Dienst $dienst; public int $dienst_id;
-    public Mannschaft $mannschaft; public int $mannschaft_id; 
+    public ?Mannschaft $mannschaft; public ?int $mannschaft_id; 
 
     public ?DateTime $anwurfVorher = null;
     public ?string $halleVorher = null;
 
     public function __construct(Dienst $dienst, Spiel $spiel_vorher){
         $this->dienst = $dienst;
-        $this->mannschaft = $dienst->mannschaft;
+        if(isset($dienst->manschaft)){
+            $this->mannschaft = $dienst->mannschaft;
+        }
         $this->anwurfVorher = $spiel_vorher->anwurf;
         $this->halleVorher = $spiel_vorher->halle;
     }
