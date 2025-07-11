49de8935f614808f21518476f392f1a9983253f0
Martin Fritz <martin@fritz.koeln>
2025-07-11 09:45:39 +0200

    kleines Refactoring mit ternärem Operator ?:

--- tests/db/WhereClause.php
+++ tests/db/WhereClause.php
@@ -190,11 +190,7 @@ class WhereClause {
             $comparator = $comparatorAndValue[0];
             $value = $comparatorAndValue[1];
             if($row[$key] instanceof DateTime){
-                if(strtoupper($value) === "CURRENT_TIMESTAMP"){
-                    $value = new DateTime("now");
-                } else {
-                    $value = new DateTime($value);
-                }
+                $value = new DateTime( strtoupper($value) === "CURRENT_TIMESTAMP" ? "now" : $value );
             }
             switch($comparator){
                 case WhereClause::LT:{
